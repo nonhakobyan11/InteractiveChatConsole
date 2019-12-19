a419855fedb7da931b0b4684c72c4c4dd357c5fe
Nonna Hakobyan <nonhakobyan11@gmail.com>
2019-12-20 02:00:27 +0400

    Adding ChatMember class with its constructor

--- src/com/homework/ChatMember.java
+++ src/com/homework/ChatMember.java
@@ -3,4 +3,15 @@ package com.homework;
 import java.util.Scanner;
 
 public class ChatMember {
+    Scanner scan = new Scanner(System.in);
+    private String nickname;
+
+    public ChatMember(){
+        System.out.println("Enter new nickname: " );
+        this.nickname = scan.nextLine();
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
 }
